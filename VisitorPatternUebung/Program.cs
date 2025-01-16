0840dbdf4e24382f8c021f0e19464bbdcb7a37a9
Jens Hidien <jhidien@proton.me>
2025-01-16 08:39:31 +0100

    cs

---
+++ VisitorPatternUebung/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace VisitorPatternUebung
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Game of Thrones - Cavalary, Archer and Spearman");
+            Console.WriteLine();
+
+            var units = new List<IUnit>
+            {
+                new Archer(),
+                new Cavalary(),
+                new Spearman()
+            };
+
+            // Move
+            var moveVisitor = new MoveAction();
+            foreach (var unit in units)
+                unit.Accept(moveVisitor);
+            
+            Console.WriteLine();
+            
+            // Defend
+            var defendVisitor = new DefendAction();
+            foreach (var unit in units)
+                unit.Accept(defendVisitor);
+            
+            Console.WriteLine();
+            
+            // Attack
+            var attackVisitor = new AttackAction();
+            foreach (var unit in units)
+                unit.Accept(attackVisitor);
+            
+            
+            Console.ReadKey();
+            return;
+        }
+    }
+}
