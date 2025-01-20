92574e5bf93d3ea89b128c1c69119a36fe8d9d17
jhi <ruebezahl1997@gmail.com>
2025-01-20 10:28:58 +0100

    update

--- InterpreterPatternUebung/Program.cs
+++ InterpreterPatternUebung/Program.cs
@@ -6,10 +6,11 @@ public class Program
     {
         Console.WriteLine("Input first number (integer): ");
         var num1 = Convert.ToInt32(Console.ReadLine());
+        
         Console.WriteLine("Input second number (integer): ");
         var num2 = Convert.ToInt32(Console.ReadLine());
+
         IExpression expr = new AddExpression(new NumberExpression(num1), new NumberExpression(num2));
-        
         Console.WriteLine($"Result: {expr.Interpret()}");
         
         Console.ReadKey();
